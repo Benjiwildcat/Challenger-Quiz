ce347343dfa9a992f65bd9ada95a0c89e4ec7025
Ben Richardson <benjiwildcat@gmail.com>
2023-04-11 23:13:45 -0500

    worked on the JavaScript

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 let quizDiv = document.querySelector("#quiz");
+let questionButton1 = document.querySelector("#answer1");
 
 let timer = 90
 
@@ -12,11 +13,13 @@ timerID = setInterval(function () {
 
 
 
-let questions = [{ question: "What was the original name of Mario?", answers: ["Redman", "Jumpman", "Balloon Fighter", "Wario"], correctAnswer: "Jumpman" },
-{ question: "What year did Super Mario Bros release in the U.S.?", answers: ["1983", "1989", "1990", "1985"], correctAnswer: "1985" },
-{ question: "What is the name of the power-up that comes from the brown leaf?", answers: ["Tanooki", "Grizzly", "Hanafunda", "Nine Tails"], correctAnswer: "Tanooki" },
-{ question: "What was Princess Peach's original name?", answers: ["Cherry", "Blossom", "Pauline", "Toadstool"], correctAnswer: "Toadstool" },
-{ question: "What year did the original Mario Kart release in the U.S.?", answers: ["1987", "1992", "1984", "1994"], correctAnswer: "1992" },
+let questions = [
+
+    { question: "What was the original name of Mario?", answers: ["Redman", "Jumpman", "Balloon Fighter", "Wario"], correctAnswer: "Jumpman" },
+    { question: "What year did Super Mario Bros release in the U.S.?", answers: ["1983", "1989", "1990", "1985"], correctAnswer: "1985" },
+    { question: "What is the name of the power-up that comes from the brown leaf?", answers: ["Tanooki", "Grizzly", "Hanafunda", "Nine Tails"], correctAnswer: "Tanooki" },
+    { question: "What was Princess Peach's original name?", answers: ["Cherry", "Blossom", "Pauline", "Toadstool"], correctAnswer: "Toadstool" },
+    { question: "What year did the original Mario Kart release in the U.S.?", answers: ["1987", "1992", "1984", "1994"], correctAnswer: "1992" },
 ]
 
 
